08295690f438511f4c0e4d6db8845f2bd35442e6
Pesec1 <danzil344@gmail.com>
2024-10-06 00:37:04 +0500

    Change gitsigns back to default one

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,15 +10,15 @@
 return {
   {
     'lewis6991/gitsigns.nvim',
+    signs = {
+      add = { text = '┃' },
+      change = { text = '┃' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+      untracked = { text = '┆' },
+    },
     opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
