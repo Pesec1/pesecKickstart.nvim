e52068d37e018051904ff45eff4c9061431860ce
Pesec1 <danzil344@gmail.com>
2024-12-19 05:23:49 +0500

    Fixed autoformat on save for specific filetype in conform

--- lua/plugins/autoformat_conform.lua
+++ lua/plugins/autoformat_conform.lua
@@ -15,14 +15,17 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true, python = true }
-      return {
-        timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
+      -- Disable autoformat on certain filetypes
+      local ignore_filetypes = { 'python', 'javascript' }
+      if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+        return
+      end
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      -- ...additional logic...
+      return { timeout_ms = 500, lsp_format = 'fallback' }
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -180,7 +180,6 @@ return {
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- tsserver = {},
 
       lua_ls = {
         -- cmd = {...},
@@ -211,7 +210,7 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
-      'eslint_d',
+      'ts_ls',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
