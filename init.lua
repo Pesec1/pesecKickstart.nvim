6b5d301c4d0301d1b64651da4805464858a82c50
Pesec1 <danzil344@gmail.com>
2025-01-04 13:41:20 +0500

    Added new autocmd to enable / disable LSP diagnostics visuals and cmp

--- init.lua
+++ init.lua
@@ -261,6 +261,7 @@ require('lazy').setup({
 
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'plugins' },
+  { import = 'autocmd' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
---
+++ lua/autocmd/disable_lsp.lua
@@ -0,0 +1,22 @@
+--NOTE: https://gist.github.com/bnse/a3eb5b9941e6af4582c5406b29d76e05
+return {
+  vim.api.nvim_create_user_command('DiagnosticToggle', function()
+    local config = vim.diagnostic.config
+    local vt = config().virtual_text
+
+    config {
+      virtual_text = not vt,
+      underline = not vt,
+      signs = not vt,
+    }
+
+    local cmp = require 'cmp'
+    if vt then
+      cmp.setup { enabled = false }
+    else
+      cmp.setup { enabled = true }
+    end
+  end, { desc = 'toggle diagnostic / autocomplete' }),
+
+  vim.keymap.set('n', '<leader>tl', '<cmd>DiagnosticToggle<CR>'),
+}
