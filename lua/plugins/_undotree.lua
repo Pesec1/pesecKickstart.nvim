253b732ad3f47883c3e8c7d6759578c69a73594d
Pesec1 <danzil344@gmail.com>
2025-01-04 14:38:38 +0500

    clean up of plugins that I do not use or use very little, next iteration of config changing if it is the same, will delete those

---
+++ lua/plugins/_undotree.lua
@@ -0,0 +1,10 @@
+--NOTE: disable for now, because it feels like it is not suitable for my workflow and it is not used enough
+return {}
+-- return {
+--   'jiaoshijie/undotree',
+--   dependencies = 'nvim-lua/plenary.nvim',
+--   config = true,
+--   keys = {
+--     { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
+--   },
+-- }
